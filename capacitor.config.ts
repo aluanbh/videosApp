08d0f36eec0305638074c8db651ac6b847618134
Andre Luan de Oliveira <aluanbh@gmail.com>
2022-02-07 18:29:58 -0300

    Adicionei o component SearchBar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'oliveira.luan.videosapp',
   appName: 'videosApp',
   webDir: 'www',
   bundledWebRuntime: false
