ed9f83572f6dbe948cdc405e7718b1aa806bcc4a
Andre Luan de Oliveira <aluanbh@gmail.com>
2022-02-08 13:20:31 -0300

    Serviço temporario e envio de dados do filme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'homem-aranha',
+    loadChildren: () => import('./filmes/homem-aranha/homem-aranha.module').then( m => m.HomemAranhaPageModule)
+  },
+  {
+    path: 'encanto',
+    loadChildren: () => import('./filmes/encanto/encanto.module').then( m => m.EncantoPageModule)
+  },
+  {
+    path: 'dados-filme',
+    loadChildren: () => import('./dados-filme/dados-filme.module').then( m => m.DadosFilmePageModule)
   }
 ];
 @NgModule({
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,9 @@
+import { DadosService } from './../services/dados.service';
 import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -19,7 +21,8 @@ export class Tab1Page {
       duracao: '2h 28m',
       classificacao: 76,
       cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg',
-      generos: ['Ação', 'Aventura', 'Ficção científica']
+      generos: ['Ação', 'Aventura', 'Ficção científica'],
+      pagina: '/homem-aranha',
     },
     {
       nome: 'Encanto (2021)',
@@ -27,11 +30,22 @@ export class Tab1Page {
       duracao: '1h 42m',
       classificacao: 76,
       cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4j0PNHkMr5ax3IA8tjtxcmPU3QT.jpg',
-      generos: ['Animação', 'Comédia', 'Família', 'Fantasia']
+      generos: ['Animação', 'Comédia', 'Família', 'Fantasia'],
+      pagina: '/encanto',
     }
   ];
 
-  constructor(public alertController: AlertController,public toastController: ToastController) {}
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController,
+    public dadosService: DadosService,
+    public route: Router
+    ) {}
+
+  exibirFilme(filme: IFilme){
+    this.dadosService.guardarDados('filme', filme);
+    this.route.navigateByUrl('/dados-filme');
+  }
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
